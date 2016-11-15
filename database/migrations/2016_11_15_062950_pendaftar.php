8ca489b589a7b4e858d3e40bbbecbf690a33fae0
vinsensiussatya <vincentsaty@gmail.com>
2016-11-15 13:36:32 +0700

    migration pendaftar

---
+++ database/migrations/2016_11_15_062950_pendaftar.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Pendaftar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftar', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('ipk');
+            $table->string('prestasi');
+            $table->string('pengalaman_kerja');
+            $table->string('pengalaman_organisasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pendaftar');
+    }
+}
